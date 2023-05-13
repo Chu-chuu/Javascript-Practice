67d7909dfadc775b4ca949f4f987a4b43db273db
Ngozi Umekwe <umekweochuwa@gmail.com>
2023-05-13 15:12:12 +0000

    First Commit

---
+++ practice.js
@@ -0,0 +1,40 @@
+let Fruits = "Favourite fruit is Grape";
+let Activities = "I love Yoga";
+let Drink = "My favourite drink in Mohito"
+let Name = "My name is Dora"
+let Food = "My favourite food is Pasta"
+
+
+// console.log(Fruits);
+// let Age = [10, 12, 14, 20, 10]
+// For int variable
+let FolaAge = 10
+let AareAge = 100
+let temp = -2
+let price = 15
+let myAge = 18
+
+// for floats
+let Math = 97.2
+let English = 70.4
+let Physics = 60.5
+let Chemistry = 90.7
+let Biology = 81.5
+
+// Boolean
+const pi = 3.14
+let x = 25
+let y = ""
+let intro = "My name is Dora"
+let a = -0
+
+document.write(Math);
+document.write(Name);
+document.write(temp);
+document.write(Food)
+document.write(Fruit)
+document.write(Age)
+document.write(Biology)
+document.write(Boolean(pi));
+document.write(Boolean(x));
+document.write(Boolean(y));
